ace2761706866b74cc2b134484f2b03d18244919
NobiSamid <nobisamid@gmail.com>
2021-11-09 15:47:34 +0600

    navigation bar customized

--- src/pages/shared/Navigation.js
+++ src/pages/shared/Navigation.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
@@ -8,49 +8,139 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import { Link, NavLink } from 'react-router-dom';
 import useAuth from '../../hooks/useAuth';
+import { styled, alpha } from '@mui/material/styles';
+import { Menu, MenuItem } from '@mui/material';
+import useMediaQuery from '@mui/material/useMediaQuery';
+import { useTheme } from '@mui/material/styles';
+
 
 const Navigation = () => {
 
-    const {user, logout} = useAuth();
+    const [anchorEl, setAnchorEl] = useState(null);
+    const { user, logout } = useAuth();
+    const theme = useTheme();
+    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+    console.log(isMobile);
+
+    const handleMenu = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+
     return (
         <Box sx={{ flexGrow: 1 }}>
             <AppBar position="static">
                 <Toolbar>
-                    <IconButton
-                        size="large"
-                        edge="start"
-                        color="inherit"
-                        aria-label="menu"
-                        sx={{ mr: 2 }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+                    <Typography variant="h6" component="div" style={{ flex: 1, textAlign: "left" }}>
                         Doc
                     </Typography>
-                    <NavLink style={{textDecoration:"none", color:'whitesmoke'}} to="/">
+                    {/* <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/">
                         Home
                     </NavLink>
-                    <NavLink style={{textDecoration:"none", color:'whitesmoke'}} to="/appoinment">
+                    <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/appoinment">
                         Appoinment
-                    </NavLink>
-                    {
+                    </NavLink> */}
+                    {/* {
                         user?.email ?
                             <Box>
-                                <NavLink style={{textDecoration:"none", color:'whitesmoke'}} to="/dashboard">
+                                <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/dashboard">
                                     Dashboard
                                 </NavLink>
                                 <Button onClick={logout} color="inherit">Log-Out</Button>
                             </Box>
-                        :
-                        <NavLink style={{textDecoration:"none", color:'whitesmoke'}} to="/login">
-                            <Button color="inherit">Log-in</Button>
-                        </NavLink>
-                    }
-                  
+                            :
+                            <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/login">
+                                <Button color="inherit">Log-in</Button>
+                            </NavLink>
+                    } */}
+                    <Box style={{ width: "40%" }}>
+                        {isMobile ? (<>
+                            <IconButton
+                                size="large"
+                                edge="end"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{ mr: 2 }}
+                                onClick={handleMenu}
+                            >
+                                <MenuIcon />
+                            </IconButton>
+                            <Menu
+                                id="menu-appbar"
+                                anchorEl={anchorEl}
+                                anchorOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                keepMounted
+                                transformOrigin={{
+                                    vertical: 'top',
+                                    horizontal: 'right',
+                                }}
+                                open={Boolean(anchorEl)}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <NavLink style={{ textDecoration: "none", color: 'black' }} to="/">
+                                        Home
+                                    </NavLink>
+                                </MenuItem>
+                                <MenuItem>
+                                    <NavLink style={{ textDecoration: "none", color: 'black' }} to="/appoinment">
+                                        Appoinment
+                                    </NavLink>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    {
+                                        user?.email ?
+                                            <>
+                                                <NavLink style={{ textDecoration: "none", color: 'black' }} to="/dashboard">
+                                                    Dashboard
+                                                </NavLink>
+                                                <Button onClick={logout} color="inherit">Log-Out</Button>
+                                            </>
+                                            :
+                                            <NavLink style={{ textDecoration: "none", color: 'black' }} to="/login">
+                                                <Button color="inherit">Log-in</Button>
+                                            </NavLink>
+                                    }
+                                </MenuItem>
+                            </Menu>
+                        </>) :
+                            (
+                                <Box style={{ display: 'flex' }} sx={{ justifyContent: 'space-evenly', alignItems: 'center' }} >
+                                    <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/">
+                                        Home
+                                    </NavLink>
+
+                                    <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/appoinment">
+                                        Appoinment
+                                    </NavLink>
+                                    {
+                                        user?.email &&
+                                        <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/dashboard">
+                                            Dashboard
+                                        </NavLink>
+                                    }
+                                    {
+                                        user?.email ?
+                                            <Button onClick={logout} color="inherit">log-out</Button>
+                                            :
+                                            <NavLink style={{ textDecoration: "none", color: 'whitesmoke' }} to="/login">
+                                                <Button color="inherit">Log-in</Button>
+                                            </NavLink>
+                                    }
+                                </Box>
+                            )}
+
+                    </Box>
+
                 </Toolbar>
             </AppBar>
-    </Box>
+        </Box>
     );
 };
 
